257aaf7ac7e5aebc3e72b8b26b9dd722598271f2
anthony <anthony.rmd@gmail.com>
2023-06-28 03:34:05 -0500

    diseño de formulario de guias

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
 
   <!-- Styles -->
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('assets/fontawesome/css/all.min.css') }}">
 </head>
 
 <body>
