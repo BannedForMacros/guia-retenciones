d6ab719cfc990ac54f18dd3f7aa577c4c68adc43
anthony <anthony.rmd@gmail.com>
2023-11-24 14:52:08 -0500

    prueba envio_2

--- app/Http/Controllers/Guia/GuiaSalidaController.php
+++ app/Http/Controllers/Guia/GuiaSalidaController.php
@@ -1389,7 +1389,7 @@ class GuiaSalidaController extends Controller
             "BienesATransportar" => $body_detalle
         ]; 
         
-        // dd(json_encode($body));
+        dd(json_encode($body));
         // dd($body);
         
         $url_button = route('guiasalida.pdfDecode', ['guia'=> $guia->id]);
