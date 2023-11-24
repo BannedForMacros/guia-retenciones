47e67f4c24a6d7c887bed839ce5aea3d35284e50
anthony <anthony.rmd@gmail.com>
2023-11-24 15:10:27 -0500

    prueba envio_9

--- app/Http/Controllers/Guia/GuiaSalidaController.php
+++ app/Http/Controllers/Guia/GuiaSalidaController.php
@@ -1390,7 +1390,7 @@ class GuiaSalidaController extends Controller
             "BienesATransportar" => $body_detalle
         ]; 
         
-        dd(json_encode($body));
+        // dd(json_encode($body));
         // dd($body);
         
         $url_button = route('guiasalida.pdfDecode', ['guia'=> $guia->id]);
@@ -1405,7 +1405,7 @@ class GuiaSalidaController extends Controller
             $send = Http::withHeaders(['Credencial' => $credencial])
                         ->asJson() // Asegurarse de que se envíe como JSON
                         // ->put("{$api_facturacion}", $body)->object();
-                        ->put("{$api_facturacion}", $body)->object();
+                        ->put("{$api_facturacion}", json_decode($body))->object();
                         // ->put('http://161.132.192.240:8180/api/Guia21', $body)->object();
             if ($send == null) {
                 $procede = false;
