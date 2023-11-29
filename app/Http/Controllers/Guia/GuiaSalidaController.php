69dabb5d6071c41b058066e91fed0af5ee90e7e8
anthony <anthony.rmd@gmail.com>
2023-11-29 15:36:27 -0500

    anular rechazadas

--- app/Http/Controllers/Guia/GuiaSalidaController.php
+++ app/Http/Controllers/Guia/GuiaSalidaController.php
@@ -132,7 +132,7 @@ class GuiaSalidaController extends Controller
             }
             $mostrar_anular = false;
 
-            if ($value->guia_estado_id == 1 or $value->guia_estado_id == 2) {
+            if ($value->guia_estado_id == 1 or $value->guia_estado_id == 2 or $value->guia_estado_id == 3) {
                 $mostrar_anular = true;
             }
 
