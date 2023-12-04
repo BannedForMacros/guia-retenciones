4ed20fdf1c01e173aaefd7297f6371907009e682
anthony <anthony.rmd@gmail.com>
2023-12-04 12:49:09 -0500

    validacion de modalidad de traslado, por loalstoarge y js

--- public/js/guias/salida/create.js
+++ public/js/guias/salida/create.js
@@ -603,6 +603,14 @@ var callStore = (guardar_avance = false) => {
         procede_store = false;
         msj_store = `<b>Las direcciones no pueden ser las mismas</b>`
       }
+
+      if (procede_store == true) {
+        if (formData.get('modalidad_traslado').tirm == '') {
+          procede_store = false;
+          msj_store = `<b>No se cargo la modalidad de traslado</b>`;
+        }
+      }
+
     }
   }
 
